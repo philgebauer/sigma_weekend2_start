bc36bf9f299b81299a1ba7af65df8b7016797019
Phil G <philg@Phils-MacBook-Pro.local>
2016-11-07 08:39:57 -0600

    added styling

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,83 +1,90 @@
-$(document).ready(function(){
-  $("#buttons").on("click", getData);
-  // getData();
-
-var currentSlide = 0;
-
-addBottom();
+$(document).ready(function() {
+    $("#buttons").on("click", getData);
 
 
-  function getData() {
-    $.ajax({
-      type: 'GET',
-      url: '/data', // http://localhost:5000/data
-      beforeSend: function() {
-          console.log('hellooo');
-      },
-      success: function(data) {
-        updateDom(data.sigmanauts);
-      },
-      error: function(xhr) {
-        console.log('request failed');
-      }
-    });
-  }
-
-$("#buttons").on("click", '#rightClick', addCurrentSlide);
-$("#buttons").on("click", '#leftClick',  subCurrentSlide);
+    var currentSlide = 0;
+    addBottom();
 
 
-
-function addCurrentSlide () {
-  currentSlide++;
-  if (currentSlide == 19) {
-    currentSlide = 1;
-  }
-
-  console.log(currentSlide);
-}
-
-function subCurrentSlide () {
-  currentSlide--;
-  if (currentSlide == 0) {
-    currentSlide = 18;
-    $(this).fadeIn();
-  }
-  console.log(currentSlide);
-}
-
-
-function updateDom(sigmanauts) {
-
-  $.each(sigmanauts, function(i, person) {
-
-    person.id = i;
-    if (currentSlide == person.id) {
-    squarecolor();
-
-    $("#person-container").empty();
-    $("#person-container").append('<div class="person"></div>');
-    var $el = $("#person-container").children().last();
-
-    $el.append('<h2>' + this.name + '</h2>');
-    $el.append('<p>' + this.git_username + '</p>');
-    $el.append('<p>' + this.shoutout + '</p>');
-
+    function getData() {
+        $.ajax({
+            type: 'GET',
+            url: '/data', // http://localhost:5000/data
+            beforeSend: function() {
+                console.log('hellooo');
+            },
+            success: function(data) {
+                updateDom(data.sigmanauts);
+            },
+            error: function(xhr) {
+                console.log('request failed');
+            }
+        });
     }
 
-  });
- }
 
-function addBottom () {
- for (var i = 1; i < 20; i++ ) {
-   $('#carousel').append('<div id="' + i + '" class="boxes"> </div>').children();
- };
-}
+    //right and left buttons. Each add to the current slide count
+    $("#buttons").on("click", '#rightClick', addCurrentSlide);
+    $("#buttons").on("click", '#leftClick', subCurrentSlide);
 
-function squarecolor () {
-  $('.boxes').css('background-color', 'white');
-  $('#' + currentSlide).css('background-color', 'red');
-}
+
+
+    //adds to current slide and checks if it is higher than 19, or less that 1
+    function addCurrentSlide() {
+        currentSlide++;
+        if (currentSlide == 19) {
+            currentSlide = 1;
+
+        }
+
+        console.log(currentSlide);
+    }
+
+    function subCurrentSlide() {
+        currentSlide--;
+        if (currentSlide == 0) {
+            currentSlide = 18;
+
+        }
+        console.log(currentSlide);
+    }
+
+
+    //appends student data to the DOM
+    function updateDom(sigmanauts) {
+        $.each(sigmanauts, function(i, person) {
+
+            person.id = i;
+            if (currentSlide == person.id) {
+                squarecolor();
+
+                $("#person-container").empty();
+                $("#person-container").append('<div class="person"></div>');
+                var $el = $("#person-container").children().last();
+
+                $el.append('<h2>' + this.name + '</h2>');
+                $el.append('<p>' + this.git_username + '</p>');
+                $el.append('<p>' + this.shoutout + '</p>');
+
+
+              }
+
+        });
+    }
+
+    //adds a unique ID to bottom bar squares.
+    function addBottom() {
+        for (var i = 1; i < 20; i++) {
+            $('#carousel').append('<div id="' + i + '" class="boxes"> </div>').children();
+        };
+    }
+
+    //changes color based on unique ID given above.
+    function squarecolor() {
+        $('.boxes').css('background-color', 'white');
+        $('#' + currentSlide).css('background-color', 'red');
+    }
+
 
 
 });
